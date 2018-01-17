70ab1f6e8ad7290e191030f3716a9ee92b4e187e
sliverTwo <18142611739@163.com>
2018-01-17 22:06:57 +0800

    Initial commit

---
+++ src/com/sliver/test/HelloIdea.java
@@ -0,0 +1,9 @@
+package com.sliver.test;
+
+
+public class HelloIdea {
+   public static void main(String[] args){
+       System.out.println("Hello Idea");
+       System.out.println("你好 Idea");
+   }
+}
